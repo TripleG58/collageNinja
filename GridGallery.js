473075c743d1dc6d1610d19044104700af8cb25a
Gerald Gehrke <TripleG58@users.noreply.github.com>
2022-02-04 03:40:08 -0600

    rewriting library

---
+++ GridGallery.js
@@ -0,0 +1,22 @@
+//
+// dependant on utility functions, "dqs" and "dqsa" being
+// declared globally in a previous script tag
+//
+
+class GridGallery {
+    #rows
+    #cols
+    #maxW
+    #minW
+    #maxH
+    #minH
+    #elem
+    #css
+    #grids
+    static #numberOfInstances = 0;
+
+    constructor(props) {
+
+    }
+
+}
